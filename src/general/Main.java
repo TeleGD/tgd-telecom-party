50531240da1360716f2e7e2f796f6fc0b410c30f
Amos George <amos.george@telecomnancy.eu>
2018-01-25 17:09:28 +0100

    initial commit

---
+++ src/general/Main.java
@@ -0,0 +1,39 @@
+package general;
+
+import java.io.File;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class Main extends StateBasedGame{
+	
+	public static int longueur=1280;
+	public static int hauteur=720;
+	
+	public static void main(String[] args) throws SlickException {
+		//Normalement c'est plus necessaire, c'est fait dans le setup du projet en theorie
+		//Et pourtant quand je cree un runnable jar il le faut sinon le jar ne se lance pas...
+		System.setProperty("org.lwjgl.librarypath", new File("natives").getAbsolutePath());
+		AppGameContainer app = new AppGameContainer(new Main(),longueur, hauteur, false);
+		app.setTargetFrameRate(60);
+		app.setVSync(true);
+		app.setShowFPS(true);
+		app.start();
+	}
+	
+
+	public Main() {
+		super("Projet_2018 #NoName");
+	}
+
+
+
+	@Override
+	public void initStatesList(GameContainer container) throws SlickException {
+		addState(new World());
+		this.enterState(World.ID);
+	}
+}
