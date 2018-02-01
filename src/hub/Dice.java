78d889d9931e288d2d2cc5dd8d79e0f1062a12f3
Xavier Indice <Xavier.Indice@telecomnancy.eu>
2018-02-01 16:16:57 +0100

    Pour des idées dans les dés

---
+++ src/hub/Dice.java
@@ -0,0 +1,31 @@
+package hub;
+
+import java.util.Random;
+import org.newdawn.slick.Image;
+
+public class Dice {
+	
+	private int nbFaces; // Nombre de faces d'un de.
+	private int[] faces; // Ensemble des valeurs des faces du de (a preciser pour elaborer des des speciaux).
+	private int num; // Derniere face sur laquelle le de est tombe.
+	
+	public Dice(int n, int[] T, int i) {
+		this.nbFaces = n;
+		this.faces = T;
+		this.num = i;
+	}
+	
+	public void roll() {
+		// Lance le de.
+		Random r = new Random();
+		this.num = this.faces[r.nextInt(this.nbFaces)];
+	}
+	
+	public int getValue() {
+		// Retourne la derniere valeur du de.
+		return this.num;
+	}
+	
+	
+
+}
