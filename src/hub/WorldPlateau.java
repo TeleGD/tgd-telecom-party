acad576d2e5e795e294afd45705200aed3827eb4
BARLOY Nathan <nathan.barloy@telecomnancy.eu>
2018-02-08 14:32:41 +0100

    je sais pas ce que c'est

--- src/hub/WorldPlateau.java
+++ src/hub/WorldPlateau.java
@@ -1,13 +1,16 @@
 package hub;
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 
 public class WorldPlateau {
 	private Case[] chemin;
+	private int[] cheminEntiers={0,0,0,0,0,0,0,0,1};
 	private int nbJoueurs;
 	private JoueurPlateau[] listeJoueurs;
-	private String[] listeCouleurs={"blue","red","yellow","green"};
 	
 	public WorldPlateau(int n) throws SlickException {
 		nbJoueurs=n;
@@ -15,6 +18,16 @@ public class WorldPlateau {
 		for (int i=0;i<n;i++) {
 			listeJoueurs[i].setSprite(new Image("images/pion" + i));
 		}
+		for (int i=0;i<cheminEntiers.length;i++) {
+			chemin[i]=new Case(cheminEntiers[i]);
+		}
 	}
 	
+	public void render(GameContainer container,StateBasedGame game, Graphics g) throws SlickException {
+		
+	}
+	
+	public void update(GameContainer container,StateBasedGame game, int delta) throws SlickException {
+		
+	}
 }
