a49351795ed2d4f62aa0cd4b26186b28001276e1
Julien ZHAN <julien.zhan@telecomnancy.eu>
2018-04-12 16:25:17 +0200

    Oui

--- src/games/clicker/Player.java
+++ src/games/clicker/Player.java
@@ -1,13 +1,25 @@
 package games.clicker;
 
+import general.AppInput;
+import general.AppPlayer;
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Player {
-	
-	public Player() {
-		
+	private String name;
+	private Color color;
+	private int controllerId;
+	private int score;
+	private int multi;
+
+	public Player(AppPlayer appPlayer) {
+		this.name = appPlayer.getName();
+		this.color = appPlayer.FILL_COLORS[appPlayer.getColorID()];
+		this.controllerId = appPlayer.getControllerID();
+		score = 0;
+		multi =0;
 	}
 	
 	public void render(GameContainer container, StateBasedGame game, Graphics context) {
@@ -17,6 +29,16 @@ public class Player {
 
 	public void update(GameContainer container, StateBasedGame game, int delta) {
 		// TODO Auto-generated method stub
-		
+		multi = 0;
+		AppInput appInput = (AppInput) container.getInput();
+		multi += appInput.isButtonPressed(AppInput.BUTTON_A,controllerId) ? 1:0;
+		multi += appInput.isButtonPressed(AppInput.BUTTON_B,controllerId) ? 1:0;
+		multi += appInput.isButtonPressed(AppInput.BUTTON_X,controllerId) ? 1:0;
+		multi += appInput.isButtonPressed(AppInput.BUTTON_Y,controllerId) ? 1:0;
+		if (multi >= 3){
+			score -= 5;
+		} else if (multi == 1){
+			score += multi;
+		}
 	}
 }
--- src/games/clicker/World.java
+++ src/games/clicker/World.java
@@ -1,5 +1,7 @@
 package games.clicker;
 
+import general.AppGame;
+import general.AppPlayer;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.state.BasicGameState;
@@ -7,9 +9,12 @@ import org.newdawn.slick.state.StateBasedGame;
 
 import general.Playable;
 
+import java.util.ArrayList;
+
 public class World extends BasicGameState implements Playable{
 
 	private final int ID;
+	private ArrayList<Player> players;
 	
 	public World(int id) {
 		this.ID=id;
@@ -30,13 +35,18 @@ public class World extends BasicGameState implements Playable{
 	@Override
 	public void update(GameContainer container, StateBasedGame game, int delta) {
 		// TODO Auto-generated method stub
-		
+		for (Player player : players){
+			player.update(container,game,delta);
+		}
 	}
 
 	@Override
 	public void initPlayers(GameContainer container, StateBasedGame game) {
-		// TODO Auto-generated method stub
-		
+		AppGame appGame = (AppGame) game;
+		players = new ArrayList<Player>();
+		for(AppPlayer player : appGame.appPlayers){
+			players.add(new Player(player)) ;
+		}
 	}
 
 	@Override
