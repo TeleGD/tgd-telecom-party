70741abdc9c47b644d2b404ea0b3f5d76ee28d8c
Julien ZHAN <julien.zhan@telecomnancy.eu>
2018-04-19 16:07:29 +0200

    Ajout d'easter egg

--- src/games/clicker/World.java
+++ src/games/clicker/World.java
@@ -4,10 +4,7 @@ import general.AppGame;
 import general.AppInput;
 import general.AppPlayer;
 import general.utils.FontUtils;
-import org.newdawn.slick.Color;
-import org.newdawn.slick.Font;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
+import org.newdawn.slick.*;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
@@ -63,7 +60,13 @@ public class World extends BasicGameState implements Playable{
 				context.setColor(Color.black);
 				context.fillRect(width/2-width/6+1,height/2-height/6+1,width/3-1,height/3-1);
 				if (nbJoueursInit == 3){
-					//lucas.draw in bottom corner
+			Image image = null;
+			try {
+				image = new Image("images/clicker/lucas.png");
+			} catch (SlickException e) {
+
+			}
+			image.draw(width*3/4-8*marge,height*3/4-13*marge);
 				}
 				for (int i = 0; i < nbJoueursInit; i++){
 					context.setColor(players.get(i).getColor());
