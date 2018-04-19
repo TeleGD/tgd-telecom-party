db04479714ac5c6f61bff4845a3ba3e9326541e3
Victor Thevenon <victor.thevenon@telecomnancy.eu>
2018-04-19 17:46:14 +0200

    El juego funciona

--- src/games/aztecPyramids/Player.java
+++ src/games/aztecPyramids/Player.java
@@ -11,22 +11,27 @@ public class Player {
 	private int value;
 	
 	private int T1;
+	private int T2;
 	private int T3;
-	private int T5;
 	private boolean onePress;
 	private boolean twoPress;
 	private boolean threePress;
 	private boolean onePressEn;
 	private boolean twoPressEn;
 	private boolean threePressEn;
+	private boolean doitchanger;
+	private boolean winner;
 
 	private World world;
 
-	public Player(World world, int T1,int T3, int T5) {
-
+	public Player(World world, int T1,int T2, int T3) {
+		
+		
+		this.doitchanger = false;
+		this.winner = false;
 		this.T1 = T1;
+		this.T2 = T2;
 		this.T3 = T3;
-		this.T5 = T5;
 		this.world = world;
 		floor = 0;
 		onePress = false;
@@ -45,20 +50,35 @@ public class Player {
 
 	}
 
+	public void isWinner() {
+		if (this.floor == 10){
+			this.winner = true;
+		}
+	}
+	
+	public boolean getWinner() {
+		return this.winner;
+	}
+	
 	public void valueChange() {
 		if (onePress == true){
 			this.value =  1;
 		}
 		else if (twoPress == true){
-			this.value =  3;
+			this.value =  2;
 		}
 		else if (threePress == true){
-			this.value =  5;
+			this.value =  3;
 		}
 	}
 
-
-
+	public void finalChange(){
+		if (this.doitchanger == true) {
+			this.value = 0;
+		}
+		this.doitchanger = false;
+	}
+	
 	public void afterChange() {
 		onePress = false;
 		twoPress = false;
@@ -69,15 +89,23 @@ public class Player {
 		return this.value;
 	}
 
-	public void isEqual(Player P1,Player P2) {
-		if (P1.getValue() == P2.getValue()){
-			P1.value=0;
-			P2.value=0;
+	public void isEqual(Player P) {
+		System.out.println("A");
+		System.out.println(this.value);
+		
+		if (this.value == P.getValue()){
+			
+			this.doitchanger=true;
 		}
+		System.out.println("B");
+		System.out.println(this.doitchanger);
 	}
 
 	public void climb(){
+		System.out.println("indigo");
+		System.out.println(this.value);
 		this.floor = this.floor + this.value;
+		System.out.println(this.floor);
 	}
 	
 	public int getFloor() {
@@ -101,22 +129,32 @@ public class Player {
 		onePressEn = false;
 		twoPressEn = false;
 }
-	
+	public String test1(){
+		return ("toto");
+	}
+	public String test2(){
+		return ("moto");
+	}
 	
 		public void keyPressed(int key, char c) {
-        
+			
+			
+				 
+			
 			
 			
 		if (threePressEn == true && twoPressEn == true && onePressEn == true){
-			if (key == T5){
+			if (key == T3){
 				threePress = true;
 				threePressEn = false;
 			}
 			else if (key == T1) {
+				System.out.println(test1());
 				onePress = true;
 				onePressEn = false;
 			}
-			else if (key == T3) {
+			else if (key == T2) {
+				System.out.println(test2());
 				twoPress = true;
 				twoPressEn = false;
 			}
